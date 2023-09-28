816a23df89a64ee87111573864e278c8d27b6bf9
samkuria <145743807+samkuria@users.noreply.github.com>
2023-09-28 06:59:50 -0700

    ADD FILES VIA UPLOAD

---
+++ Hello World/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a, b, c;
+    printf("5");
+    scanf("%d", &a);
+    printf("7");
+    scanf("%d", &b);
+    c = a + b;
+    printf("%d + %d = %d/n", a, b, c);
+    return 0;
+}
